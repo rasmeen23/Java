1488f7d4f04b1988d8cb144a9ae3ca361ecd0b8f
rasmeen23 <rasmeenhasana0723@gmail.com>
2025-11-02 21:51:29 +0530

    file added

---
+++ Javatraining/src/com/Day2/hw1.java
@@ -0,0 +1,11 @@
+package com.Day2;
+
+public class hw1 {
+    public static void main(String[]args){
+        int num = 7; // you can change this number
+        if (num % 2 == 0)
+            System.out.println(num + " is even.");
+        else
+            System.out.println(num + " is odd.");
+    }
+}
