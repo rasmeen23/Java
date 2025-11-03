8b49a595af5e9c07fe94a1fbab31ac496920bf77
rasmeen23 <rasmeenhasana0723@gmail.com>
2025-11-03 09:26:55 +0530

    file added

---
+++ Javatraining/src/com/Day2/hw4.java
@@ -0,0 +1,12 @@
+package com.Day2;
+
+public class hw4 {
+    public static void main(String[]args){
+        if(args.length > 0){
+            System.out.println(" welcome " + args[0]);
+        }
+        else{
+            System.out.println("No name provided!");
+        }
+    }
+}
