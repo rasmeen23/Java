6fd775735bdebe61de129c3d45d42b267c1833ac
rasmeen23 <rasmeenhasana0723@gmail.com>
2025-11-03 09:12:11 +0530

    file added

--- Javatraining/src/com/Day2/hw3.java
+++ Javatraining/src/com/Day2/hw3.java
@@ -1,5 +1,19 @@
 package com.Day2;
 
+import java.util.Scanner;
+
 public class hw3 {
-    
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int num = sc.nextInt();
+        if (num > 0) {
+            System.out.println("The number is Positive.");
+        } else if (num < 0) {
+            System.out.println("The number is Negative.");
+        } else {
+            System.out.println("The number is Zero.");
+        }
+
+    }
 }
