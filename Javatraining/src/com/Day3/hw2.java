7ed7ea4c44b96979de9b235f4f28870d0ea0c6cb
rasmeen23 <rasmeenhasana0723@gmail.com>
2025-11-06 10:30:09 +0530

    file added

---
+++ Javatraining/src/com/Day3/hw2.java
@@ -0,0 +1,18 @@
+package com.Day3;
+
+public class hw2 {
+        public static void main(String[] args) {
+            // Initialize two character variables
+            char ch1 = 's';
+            char ch2 = 'e';
+
+            // Compare and display in alphabetical order
+            if (ch1 < ch2) {
+                System.out.println(ch1 + "," + ch2);
+            } else {
+                System.out.println(ch2 + "," + ch1);
+            }
+        }
+    }
+
+}
